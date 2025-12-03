09d2bc697ffe711dfa4804b7d441d7793915dcc6
alexandreberdejo <alexandre.berdejo@laplateforme.io>
2025-12-03 16:16:39 +0100

    ajoue de la condition de victoire avec les multiples combinaisons

--- Tik-Tak-Toe.py
+++ Tik-Tak-Toe.py
@@ -5,3 +5,16 @@ def afficher_plateau(board):
         if i <6:
             print("--+---+--")
     print()
+
+def victoire(board,s):
+    combinaisons=[[0,1,2],[3,4,5][6,7,8],
+                [0,3,6], [1,4,7], [2,5,8],
+                [0,4,8], [2,4,6]
+]
+
+    for i in combinaisons:
+        if board[i[0]]==board[i[1]] == board[i[2]] == s:
+            return True
+        return False
+
+
