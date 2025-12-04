d855f9c4291cf00a2077f68259d1df060b5b176b
alexandreberdejo <alexandre.berdejo@laplateforme.io>
2025-12-04 15:44:32 +0100

    finnalisation du code avec remise en forme de l'algorythme et ajoue de commentaire pour séparer les differente fonction étape future = pygames le code et fini il faut le modifier pour le mettre en résonnance avec pygames

--- Tik-Tak-Toe.py
+++ Tik-Tak-Toe.py
@@ -1,81 +1,157 @@
-def afficher_plateau(board):
-    print("\n")
-    for i in range(0,9,3):
-        print(board[i],"|",board[i+1],"|",board[i+2])
-        if i <6:
-            print("--+---+--")
-    print()
-
-def victoire(board,s):
-    combinaisons=[[0,1,2],[3,4,5],[6,7,8],
-                [0,3,6], [1,4,7], [2,5,8],
-                [0,4,8], [2,4,6]
-]
-
-    for i in combinaisons:
-        if board[i[0]]==board[i[1]] == board[i[2]] == s:
-            return True
-        return False
-
-
-
 import random
 
-def ia_aleatoire(board):
+
+# IA NIVEAU 1 : Aléatoire
+
+def ia_niveau_1(board, signe):
     cases_vides = [i for i in range(9) if board[i] == " "]
     return random.choice(cases_vides)
 
-# Les autres fonctions sont identiques à la version 1
+
+
+# IA NIVEAU 2 : Gagner / Bloquer 
 
 def ia_niveau_2(board, signe):
-    adv = "O" if signe == "X" else "X"
-    combos = [
+    adversaire = "O" if signe == "X" else "X"
+
+    combinaisons = [
         [0,1,2], [3,4,5], [6,7,8],
         [0,3,6], [1,4,7], [2,5,8],
         [0,4,8], [2,4,6]
     ]
 
-    # Tentative de gagner
-    for a,b,c in combos:
-        if board[a]==signe and board[b]==signe and board[c]==" ": return c
-        if board[a]==signe and board[c]==signe and board[b]==" ": return b
-        if board[b]==signe and board[c]==signe and board[a]==" ": return a
 
-    # Bloquer
-    for a,b,c in combos:
-        if board[a]==adv and board[b]==adv and board[c]==" ": return c
-        if board[a]==adv and board[c]==adv and board[b]==" ": return b
-        if board[b]==adv and board[c]==adv and board[a]==" ": return a
+    for combo in combinaisons:
+        a, b, c = combo
+        if board[a] == signe and board[b] == signe and board[c] == " ":
+            return c
+        if board[a] == signe and board[c] == signe and board[b] == " ":
+            return b
+        if board[b] == signe and board[c] == signe and board[a] == " ":
+            return a
 
-    # Sinon hasard
-    return random.choice([i for i in range(9) if board[i]==" "])
+    # 2) Bloquer l’adversaire
+    for combo in combinaisons:
+        a, b, c = combo
+        if board[a] == adversaire and board[b] == adversaire and board[c] == " ":
+            return c
+        if board[a] == adversaire and board[c] == adversaire and board[b] == " ":
+            return b
+        if board[b] == adversaire and board[c] == adversaire and board[a] == " ":
+            return a
+
+    # 3) Jouer au hasard
+    cases_vides = []
+    for i in range(9):
+        if board[i] == " ":
+            cases_vides.append(i)
+
+    return random.choice(cases_vides)
+
+
+
+# IA NIVEAU 3 : Stratégique
 
 def ia_niveau_3(board, signe):
-    # Centre
     if board[4] == " ":
         return 4
 
-    # Coins
-    for c in [0,2,6,8]:
-        if board[c] == " ":
-            return c
+    coins = [i for i in [0, 2, 6, 8] if board[i] == " "]
+    if coins:
+        return random.choice(coins)
 
-    # Bords
-    for b in [1,3,5,7]:
-        if board[b] == " ":
-            return b
+    bords = [i for i in [1, 3, 5, 7] if board[i] == " "]
+    if bords:
+        return random.choice(bords)
 
-niveau = int(input("Choisir niveau IA : 1, 2 ou 3 : "))
+    return ia_niveau_1(board, signe)
 
-if niveau == 1:
-    choix = ia_aleatoire(board)
-elif niveau == 2:
-    choix = ia_niveau_2(board, "O")
-else:
-    choix = ia_niveau_3(board, "O")
+
+# ---------------------------------
+# Choix IA selon niveau
+# ---------------------------------
+def ordinateur(board, signe, niveau):
+    if niveau == 1:
+        return ia_niveau_1(board, signe)
+    if niveau == 2:
+        return ia_niveau_2(board, signe)
+    if niveau == 3:
+        return ia_niveau_3(board, signe)
+    return ia_niveau_1(board, signe)
+
+
+# 
+# Affichage du plateau
+
+def afficher_plateau(board):
+    print("\n")
+    for i in range(0, 9, 3):
+        print(f" {board[i]} | {board[i+1]} | {board[i+2]} ")
+        if i < 6:
+            print("---+---+---")
+    print("\n")
+
+
+
+# Vérifier la victoire
+
+def victoire(board, s):
+    combinaisons = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+    for combo in combinaisons:
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] == s:
+            return True
+    return False
+
+
+# 
+# Match nul
+
+def match_nul(board):
+    return " " not in board
+
+
+
+# Tour du joueur
+
+def tour_joueur(board, signe):
+    while True:
+        try:
+            choix = int(input(f"Joueur {signe}, choisissez une case (0-8) : "))
+            if choix < 0 or choix > 8:
+                print("Veuillez choisir un nombre entre 0 et 8.")
+            elif board[choix] != " ":
+                print("Cette case est déjà prise.")
+            else:
+                board[choix] = signe 
+                break
+        except:
+            print("Entrée invalide. Essayez encore.")
+
+
+# Tour ordinateur
+
+def tour_ordinateur(board, signe, niveau):
+    print(f"L'ordinateur ({signe}) réfléchit (IA niveau {niveau})...")
+    choix = ordinateur(board, signe, niveau)
+    board[choix] = signe
+    print(f"L’ordinateur joue en case {choix}.")
+
+
+# JEU PRINCIPAL
 
 def jeu():
-    mode = input("1: 2 joueurs | 2: Contre IA : ")
+    print(" TIC TAC TOE ")
+
+    mode = input("Mode de jeu : (1) 2 joueurs  |  (2) contre l'ordinateur : ")
+
+    niveau_ai = 1
+    if mode == "2":
+        niveau_ai = int(input("Choisissez niveau IA : 1 (facile), 2 (moyen), 3 (difficile) : "))
+
     board = [" "] * 9
     joueur = "X"
 
@@ -83,21 +159,22 @@ def jeu():
         afficher_plateau(board)
 
         if mode == "2" and joueur == "O":
-            choix = ia_niveau_2(board, "O")
+            tour_ordinateur(board, joueur, niveau_ai)
         else:
-            choix = int(input(f"Joueur {joueur} choisissez (0-8) : "))
-
-        if board[choix] != " ":
-            print("Case occupée")
-            continue
-
-        board[choix] = joueur
+            tour_joueur(board, joueur)
 
         if victoire(board, joueur):
             afficher_plateau(board)
-            print("Victoire de", joueur)
+            print(f"Le joueur {joueur} a gagné !")
+            break
+
+        if match_nul(board):
+            afficher_plateau(board)
+            print("Match nul !")
             break
 
         joueur = "O" if joueur == "X" else "X"
-jeu()
 
+
+# Lancer le jeu
+jeu()
