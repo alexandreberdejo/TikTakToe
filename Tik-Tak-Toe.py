da57283c84e0f3a4055eac50d2658a54d17182ed
alexandreberdejo <alexandre.berdejo@laplateforme.io>
2025-12-04 15:38:22 +0100

    mise en place d'un algorythme qui permet de créé une ia de niveau 2 et mise a jour du menu avec choix du niveau et du format de jeu

--- Tik-Tak-Toe.py
+++ Tik-Tak-Toe.py
@@ -25,31 +25,54 @@ def ia_aleatoire(board):
 
 # Les autres fonctions sont identiques à la version 1
 
+def ia_niveau_2(board, signe):
+    adv = "O" if signe == "X" else "X"
+    combos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+
+    # Tentative de gagner
+    for a,b,c in combos:
+        if board[a]==signe and board[b]==signe and board[c]==" ": return c
+        if board[a]==signe and board[c]==signe and board[b]==" ": return b
+        if board[b]==signe and board[c]==signe and board[a]==" ": return a
+
+    # Bloquer
+    for a,b,c in combos:
+        if board[a]==adv and board[b]==adv and board[c]==" ": return c
+        if board[a]==adv and board[c]==adv and board[b]==" ": return b
+        if board[b]==adv and board[c]==adv and board[a]==" ": return a
+
+    # Sinon hasard
+    return random.choice([i for i in range(9) if board[i]==" "])
+
+
 def jeu():
+    mode = input("1: 2 joueurs | 2: Contre IA : ")
     board = [" "] * 9
     joueur = "X"
 
     while True:
         afficher_plateau(board)
 
-        if joueur == "X":
-            choix = int(input("Votre choix (0-8) : "))
+        if mode == "2" and joueur == "O":
+            choix = ia_niveau_2(board, "O")
         else:
-            print("L’ordinateur joue…")
-            choix = ia_aleatoire(board)
-            print("IA joue case :", choix)
+            choix = int(input(f"Joueur {joueur} choisissez (0-8) : "))
 
-        if board[choix] == " ":
-            board[choix] = joueur
-        else:
+        if board[choix] != " ":
             print("Case occupée")
             continue
 
+        board[choix] = joueur
+
         if victoire(board, joueur):
             afficher_plateau(board)
-            print(joueur, "gagne !")
+            print("Victoire de", joueur)
             break
 
         joueur = "O" if joueur == "X" else "X"
-
 jeu()
+
